4d93d5c5bc8cdb56e37ff31decf72ca0c69a31f2
Carl Christensen <carlgt1@cmc-mbp13.home>
2015-01-19 09:20:54 -0500

    fix archive dump

--- server/sql/stats_archive_trigger.sql
+++ server/sql/stats_archive_trigger.sql
@@ -7,7 +7,7 @@ SELECT r.id resultid,       exp(-(abs(unix_timestamp()-max(t.time_received))*0.6
            JOIN sensor_archive.qcn_trigger t ON r.name=t.result_name
          WHERE f.resultid IS NULL AND q.result_name IS NULL AND t.runtime_clock>0
                    GROUP BY r.id
-INTO OUTFILE 'sensor_archive_stats_dump.sql'
+INTO OUTFILE 'sensor_archive_stats_dump.sql';
 
 SELECT r.id resultid,       exp(-(abs(unix_timestamp()-max(t.time_received))*0.69314718/604800.0)) weight,         (50.0*IF(MAX(t.runtime_clock)>100000.0,100000.0,MAX(t.runtime_clock)))/86400.0 total_credit,           max(t.time_received)
          FROM continual.result r
@@ -16,6 +16,6 @@ SELECT r.id resultid,       exp(-(abs(unix_timestamp()-max(t.time_received))*0.6
            JOIN continual_archive.qcn_trigger t ON r.name=t.result_name
          WHERE f.resultid IS NULL AND q.result_name IS NULL AND t.runtime_clock>0
                    GROUP BY r.id
-INTO OUTFILE 'continualr_archive_stats_dump.sql'
+INTO OUTFILE 'continualr_archive_stats_dump.sql';
 
 
