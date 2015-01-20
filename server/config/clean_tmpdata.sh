661b02a7c8a984db84a45cd17a3de5ec47e3be28
Carl Christensen <carlgt1@cmc-mbp13.home>
2015-01-20 14:17:19 -0500

    web update

--- server/config/clean_tmpdata.sh
+++ server/config/clean_tmpdata.sh
@@ -4,3 +4,11 @@ rm -rf /var/www/boinc/continual/html/stats_*
 rm -f /var/www/boinc/sensor/html/user/data/*
 rm -f /var/www/boinc/continual/html/user/data/*
 rm -f /tmp/tmpFile*
+cd /var/www/boinc/sensor
+bin/stop
+rm -f /var/www/boinc/sensor/log_qcn-web/*
+bin/start
+cd /var/www/boinc/continual
+bin/stop
+rm -f /var/www/boinc/continual/log_qcn-web/*
+bin/start
--- server/html/user/earthquake_email.php
+++ server/html/user/earthquake_email.php
@@ -1,15 +1,15 @@
 <?php
 chdir("/var/www//boinc/sensor/html/user");
 require_once("/var/www//boinc/sensor/html/inc/earthquake_email.inc");
-$mag  = 3.2; 
-$longitude = -122.426; 
-$latitude = 38.041; 
-$depth = 18.9; 
-$n_stations = 9; 
-$etime = 1396296903.439055; 
-$dtime = 1396296925; 
-$dt_detect  = 21.6; 
-$edir       = 1396296903; 
+$mag  = 3.8; 
+$longitude = 121.674; 
+$latitude = 24.398; 
+$depth = 41.8; 
+$n_stations = 8; 
+$etime = 1420606119.193318; 
+$dtime = 1420606209; 
+$dt_detect  = 89.8; 
+$edir       = 1420606119; 
 
 earthquake_email($mag,$longitude,$latitude,$depth,$n_stations,$etime,$edir,$dtime,$dt_detect);
 
