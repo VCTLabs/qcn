39758d54f6c2ac80793b914d8e8eeabf46f645d8
Carl Christensen <carlgt1@hotmail.com>
2016-06-03 18:22:43 -0400

    new web backup script

--- server/config/backupweb.sh
+++ server/config/backupweb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-/bin/rm -f /tmp/qcn-web-backup.tgz
+FILE_BACKUP=/data/QCN/qcn-web-backup-usc.tgz
+/bin/rm -f $FILE_BACKUP
 cd /var/www
-/bin/nice -n19 /bin/tar -czf /tmp/qcn-web-backup.tgz \
+/bin/nice -n19 /bin/tar -czf $FILE_BACKUP \
   --exclude "boinc/sensor/pid_qcn-web/*" \
   --exclude "boinc/sensor/pid_qcn-web" \
   --exclude "boinc/sensor/log_qcn-web/*" \
@@ -20,9 +21,10 @@ cd /var/www
   --exclude "boinc/continual/tmp_qcn-web" \
   --exclude "boinc/continual/html/cache/*" \
   --exclude "boinc/continual/html/cache" \
- boinc/ qcn/ qcnwp/ taskfreak-0.6.4/ \
+ boinc/ qcn/ qcnwp/ \
  1>/root/backupweb.log 2>/root/backupweb.err
 # note for the following line need to have .ssh id between root & carlgt1 setup
 # also make sure that '.ssh/known_hosts' is setup to receive connections
-/bin/nice -n19 /usr/bin/scp /tmp/qcn-web-backup.tgz carlgt1@data-private:
-/bin/nice -n19 /bin/mv -f /tmp/qcn-web-backup.tgz /data/QCN/
+#/bin/nice -n19 /usr/bin/scp /tmp/qcn-web-backup.tgz carlgt1@data-private:
+#/bin/nice -n19 /bin/mv -f /tmp/qcn-web-backup.tgz /data/QCN/
+
