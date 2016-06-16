5fcbb6a0a8e656c6d79a17a45c3756d35469a602
Carl Christensen <Carl Christensen>
2016-06-15 22:23:35 -0400

    Windows client version 8.07

--- version.h
+++ version.h
@@ -7,12 +7,12 @@
 #define QCN_MAJOR_VERSION 8
 
 /* Minor part of QCN version number */
-#define QCN_MINOR_VERSION 06 
+#define QCN_MINOR_VERSION 07 
 
 /* Release part of QCN version number */
 #define QCN_RELEASE 
 
 /* String representation of QCN version number */
-#define QCN_VERSION_STRING "8.06"
+#define QCN_VERSION_STRING "8.07"
 
 #endif /* #ifndef QCN_VERSION_H */
